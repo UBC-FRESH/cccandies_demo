17028867fd84f3dc950d7f89ec756feffa1806b7
Gregory Paradis <gregory.paradis@ubc.ca>
2025-04-17 21:43:09 +0000

    update main R script

--- spades_ws3_demo.R
+++ spades_ws3_demo.R
@@ -3,65 +3,86 @@
 library(reticulate)
 library(SpaDES)
 
+################################################################################
+# set up SpaDES paths
 setPaths(modulePath = 'modules',
          inputPath = 'input',
          outputPath = 'output',
          cachePath = 'cache')
 paths <- getPaths()
+################################################################################
 
-modules <- list('spades_ws3_dataInit')
+################################################################################
+# define SpaDES module import list
+modules <- list('spades_ws3_dataInit', 'bogus_fire', 'spades_ws3')
+################################################################################
 
+################################################################################
+# define local variables (spades_ws3 module parameters)
 base.year <- 2020   
-#basenames <- c("tsa08", "tsa16", "tsa24", "tsa40", "tsa41")
-basenames <- list(c("tsa41"))
-horizon <- 1
-times <- list(start = 0, end = horizon - 1)
-tifPath = "tif"
-hdtPath = "hdt"
-hdtPrefix = "hdt_"
-outputs <-data.frame(objectName = "landscape")
-
-#scheduler.mode <- "areacontrol"
-scheduler.mode <- "optimize"
-target.masks <- list(c('? ? ? ?'))
-target.areas <- list(1000)
+# basenames <- c("tsa08", "tsa16", "tsa24", "tsa40", "tsa41") # data included!
+basenames <- list(c("tsa41")) # only run one TSA as a test (faster and simpler)
+horizon <- 3 # this would typically be one or two rotations (10 or 20 periods)
+period_length <- 10 # do not modify this unless you know what you are doing
+times <- list(start = 0, end = horizon - 1) # do not modify
+tifPath <- "tif" # do not modify (works with included dataset)
+outputs <-data.frame(objectName = "landscape") # do not modify
+scheduler.mode <- "areacontrol" # self-tuning oldest-first priority queue heuristic algorithm (should "just work")
+# scheduler.mode <- "optimize" # this should also "just work" (needs more testing)
+target.masks <- list(c('? ? ? ?')) # do not modify
 target.scalefactors <- NULL
+################################################################################
 
+################################################################################
+# define SpaDES params input arg data structure
 params <- list(spades_ws3_dataInit = list(basenames = basenames,
                                           tifPath = tifPath,
-                                          hdtPath = hdtPath,
-                                          hdtPrefix = hdtPrefix,
                                           base.year = base.year,
                                           .saveInitialTime = 0,
                                           .saveInterval = 1,
                                           .saveObjects = c("landscape"),
                                           .savePath = file.path(paths$outputPath, "landscape")),
                spades_ws3 = list(basenames = basenames,
-                                 scenario.name = scenario.name,
-                                 cbird = cbird,
-                                 horizon = 8,
-                                 tifPath = tifPath,
-                                 hdtPath = hdtPath,
-                                 hdtPrefix = hdtPrefix,
+                                 horizon = 3,
+                                 period_length = period_length,
+                                 tif.path = tif.path,
+                                 shp.path = shp.path,
                                  base.year = base.year,
                                  scheduler.mode = scheduler.mode,
                                  target.masks = target.masks,
-                                 target.areas = target.areas,
-                                 target.scalefactors = target.scalefactors))
+                                 target.scalefactors = target.scalefactors),
+               bogus_fire = list(p.to.zero = 0.99))
 sim <- simInit(paths=paths, modules=modules, times=times, params=params, outputs=outputs)
+################################################################################
+
+################################################################################
+# run SpaDES simulation
 simOut <- spades(sim, debug=TRUE)
+#
 
+################################################################################
+# plot something ?
+freq(sim$landscape$age) # this is not a great example (do better)
+################################################################################
+
+################################################################################
+# example showing how it is possible to poke around the ws3 model directly
+# py$simulate_harvest(sim$fm, list(c("tsa41")), 2020)
+################################################################################
+
+
+################################################################################
+# compile some aggregated data tables from raw geotiff spades_ws3 model output
 #years <- 2020:2099
-
 #burned.area.tsa08 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa08/projected_fire_', x, '.tif')), sum) * 6.25)})
 #burned.area.tsa16 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa16/projected_fire_', x, '.tif')), sum) * 6.25)})
 #burned.area.tsa24 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa24/projected_fire_', x, '.tif')), sum) * 6.25)})
 #burned.area.tsa40 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa40/projected_fire_', x, '.tif')), sum) * 6.25)})
 #burned.area.tsa41 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa41/projected_fire_', x, '.tif')), sum) * 6.25)})
-
 #harvested.area.tsa08 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa08/projected_harvest_', x, '.tif')), sum) * 6.25)})
 #harvested.area.tsa16 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa16/projected_harvest_', x, '.tif')), sum) * 6.25)})
 #harvested.area.tsa24 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa24/projected_harvest_', x, '.tif')), sum) * 6.25)})
 #harvested.area.tsa40 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa40/projected_harvest_', x, '.tif')), sum) * 6.25)})
 #harvested.area.tsa41 <- sapply(years, function(x){return(cellStats(raster(paste0('input/tif/tsa41/projected_harvest_', x, '.tif')), sum) * 6.25)})
+################################################################################
 
